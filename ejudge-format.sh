d71b9d3244e06cf4a05423399bcc0c2ebef7919c
Hayk Saribekyan <hayk.saribekyan@gmail.com>
2020-05-17 19:11:17 +0100

    ejudge formatting script added

--- ejudge-format.sh
+++
@@ -1,4 +0,0 @@
-# TODO: convert the list of following problems into an ejudge format.
-# - move tests into appropriate format
-# - move checkers and compile them
-# - throw errors for the problems that couldn't do
---
+++ ejudge_format.py
@@ -0,0 +1,169 @@
+import sys
+import argparse
+import os
+from os import path
+import string
+import shutil
+
+src_in = ""
+src_out = ".a"
+
+dst_in = ".in"
+dst_out = ".out"
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert contest to the ejudge default format.')
+
+    parser.add_argument('ejudge', type=str,
+                        help='path to the ejudge contest root')
+
+    parser.add_argument('--tex', type=str,
+                        dest='texfile',
+                        help='path to the main statements.tex; converts all problems')
+
+    parser.add_argument('--prob_path', dest='problem_path',
+                        help='path to a problem to be converted')
+
+    parser.add_argument('--letter', dest='letter',
+                        help='letter to convert a problem to')
+
+    args = parser.parse_args()
+
+    if not path.isdir(args.ejudge):
+        print("No ejudge dir found... qutting...")
+        exit(1)
+
+    if not ( (args.texfile is None) ^ ( args.problem_path is None) ):
+        print("Either give a problem or a tex file... not both and not none... quitting...")
+        exit(1)
+
+    if (args.problem_path is not None):
+        args.problem_path = path.abspath(args.problem_path)
+        if (args.letter is None):
+            print("You gave me a problem, but not a letter... quitting...")
+            exit(1)
+        
+        if (args.letter not in string.ascii_uppercase):
+            print("The letter is not an ascii uppercase... quitting...")
+            exit(1)
+
+        if not path.isdir(args.problem_path):
+            print("No such problem %s... quitting..." % args.problem_path)
+            exit(1)
+
+        args.problems_base = path.dirname(args.problem_path)
+        args.problem_name = path.basename(args.problem_path)
+
+    else:
+        args.texfile = path.abspath(args.texfile)
+        if not path.isfile(args.texfile):
+            print("No such file %s... quitting..." % args.texfile)
+            exit(1)
+
+        args.contest_base = path.dirname(path.dirname(args.texfile))
+        args.problems_base = path.join(args.contest_base, 'problems')
+
+    if not path.isdir(args.problems_base):
+        print("No problem base found at %s... quitting..." % args.problems_base)
+        exit(1)
+    
+    print("Parsed successfully")
+    print()
+    return args
+
+def get_problems_from_tex(texfile):
+    problem_list = [ ]
+    letter_id = 0
+    with open(texfile, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('\\includeproblem{'):
+                st = line.find('{')
+                en = line.find('}')
+                problem_list.append(
+                    (line[st + 1:en], string.ascii_uppercase[letter_id]))
+                letter_id += 1
+    return problem_list
+
+def convert_problem(problems_base, problem, ejudge, letter):
+    print("Creating problem %s -> %s" % (problem, letter))
+    problem_path = path.join(problems_base, problem)
+
+    dest_dir = path.join(ejudge, 'problems', letter)
+
+    if path.isdir(dest_dir):
+        print("Destination directory exists for problem %s. Overwrite? y/n" % letter)
+        ans = sys.stdin.readline()
+        if ans[0] != 'y':
+            print('Ok, skipping problem %s -> %s' % (problem, letter))
+            print()
+            return
+        else:
+            shutil.rmtree(dest_dir)
+
+    dest_tests_dir = path.join(dest_dir, "tests")
+    os.makedirs(dest_tests_dir)
+
+    src_tests_dir = path.join(problem_path, "tests")
+
+    tests_list = os.listdir(src_tests_dir)
+    print(tests_list)
+
+    n_moved = 0
+    for f in tests_list:
+        try:
+            test = f.split('.')[0]
+            if 0 < int(test) <= 999:
+                if f == test + src_in:
+                    shutil.copy(path.join(src_tests_dir, f),
+                                path.join(dest_tests_dir, test + dst_in))
+                    n_moved += 1
+                elif f == test + src_out:
+                    shutil.copy(path.join(src_tests_dir, f),
+                                path.join(dest_tests_dir, test + dst_out))
+                    n_moved += 1
+                    
+        except:
+            pass
+    
+    print("  %d test files moved" % n_moved)
+    if n_moved % 2 != 0:
+        print("  WARNING - odd number of test files")
+
+    checkers_dir = path.join(ejudge, 'checkers')
+    if not path.isdir(checkers_dir):
+        os.mkdir(checkers_dir)
+
+    small_letter = letter.lower()
+
+    for prog in ['interactor', 'checker', 'check']:
+        src_file = path.join(problem_path, prog + '.cpp')
+        if path.isfile(src_file):
+            print("Looks like %s has %s.cpp . Copying to ejudge" % (problem, prog))
+            if prog == 'interactor':
+                dest_fname = 'interactor'
+            else:
+                dest_fname = 'check'
+            dest_fname += '_' + small_letter + '.cpp'
+
+            dst_file = path.join(dest_dir, dest_fname)
+            shutil.copy(src_file, dst_file)
+
+            dst_file = path.join(checkers_dir, dest_fname)
+            shutil.copy(src_file, dst_file)
+
+    print("Done problem %s -> %s" % (problem, letter))
+    print()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.texfile is not None:
+        problems_to_convert = get_problems_from_tex(args.texfile)
+    else:
+        problems_to_convert = [ (args.problem_name, args.letter) ]
+
+    for problem, letter in problems_to_convert:
+        convert_problem(
+            args.problems_base, problem,
+            args.ejudge,
+            letter)
